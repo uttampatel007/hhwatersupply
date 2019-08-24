15f72aaa2e6a1b2de62bc9773cd82a4d6cb36e6c
UttamVelani <40419289+uttampatel007@users.noreply.github.com>
2019-08-24 21:25:12 +0530

    Update settings.py

--- hhwater/settings.py
+++ hhwater/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't#emz*t*1d=hvek)n73!2q)g8x%lw^6n0kq)cra=3)8ytpuy0#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
 EMAIL_HOST_USER = 'suryamailservice@gmail.com'
-EMAIL_HOST_PASSWORD = '112651998uttam'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True    
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
